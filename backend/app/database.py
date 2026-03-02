db86595728fe18592b9bb650106007be9900e3dc
Xhulia Jasimi <xhulia.jasimi@siemens.com>
2026-03-02 22:34:27 +0100

    fix port mapping format

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432").split(":")[0]
 POSTGRES_DB = os.getenv("POSTGRES_DB", "todos")
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
